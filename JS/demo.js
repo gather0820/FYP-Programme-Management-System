eb2c668aed36e9326bd452d4b954e1d45f057f39
gather0820 <gather0820@users.noreply.github.com>
2020-02-20 11:24:32 +0000

    Create demo.js

---
+++ JS/demo.js
@@ -0,0 +1,21 @@
+//document.write("hello");
+//document.getElementById("d1")  //查找element
+//声明变量 使用var
+window.onload=function(){  //页面加载完成之后
+    // var i = document.getElementById("d1")
+    // i.innerHTML="Welcome to the NewYork";
+    // var i=document.getElementById("p2");
+    // i.style.color = "red";
+    this.document.getElementById("h1").innerHTML="text";
+}
+document.write(Date());
+
+// function changeImage(){
+//     var ielm = document.getElementById("Img1");
+//     if(ielm.src.match("1")){
+//         ielm.src="img/2.jpg";
+//     }else{
+//         ielm.src="img/1.jpg";
+//     }
+// }
+
