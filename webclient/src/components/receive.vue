e86c3ac600950ffb8a9a72597fd3b844b4e5f169
gather0820 <gather0820@users.noreply.github.com>
2020-03-26 13:58:11 +0000

    Create receive.vue

---
+++ webclient/src/components/receive.vue
@@ -0,0 +1,26 @@
+<template>
+<!-- <div> -->
+    <el-container>
+        <el-aside>
+            
+        </el-aside>
+        <el-main>
+             
+        </el-main>
+    </el-container>
+<!-- </div> -->
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
