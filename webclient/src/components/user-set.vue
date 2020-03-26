0a67c3acb0db110a687c5d1cc276d2265941a17e
gather0820 <gather0820@users.noreply.github.com>
2020-03-26 13:59:05 +0000

    Create user-set.vue

---
+++ webclient/src/components/user-set.vue
@@ -0,0 +1,123 @@
+<template>
+<div>
+    <div>
+        <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="change-password">
+            <el-form-item label="Old:" prop="oldPass">
+                <el-input type="password" v-model="ruleForm.oldPass" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="New:" prop="newPass">
+                <el-input type="password" v-model="ruleForm.newPass" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="Confirm:" prop="checkNewPass">
+                <el-input type="password" v-model="ruleForm.checkNewPass" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="submitForm('ruleForm')">submit</el-button>
+                <el-button @click="resetForm('ruleForm')">reset</el-button>
+            </el-form-item>
+        </el-form>
+
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        //  验证原密码
+        let validateOldPass = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('Input your old password!'))
+            } else {
+                if (this.ruleForm.oldPass !== '') {
+                    this.$refs.ruleForm.validateField('newPass')
+                }
+                callback()
+            }
+        }
+        //  验证新密码
+        var validateNewPass = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('Input a new password!'))
+            } else if (value === this.ruleForm.oldPass) {
+                callback(new Error('Your input a new password!'))
+            } else {
+                callback()
+            }
+        }
+        //  验证再次输入密码
+
+        var validateCheckNewPass = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('input again to confirm!'))
+            } else if (value !== this.ruleForm.newPass) {
+                callback(new Error('The two passwords you typed do not match!!'))
+            } else {
+                callback()
+            }
+        }
+
+        return {
+            ruleForm: {
+                oldPass: '',
+                newPass: '',
+                checkNewPass: ''
+            },
+            rules: {
+                oldPass: [{
+                    validator: validateOldPass,
+                    trigger: 'blur'
+                }],
+                newPass: [{
+                    validator: validateNewPass,
+                    trigger: 'blur'
+                }],
+                checkNewPass: [{
+                    validator: validateCheckNewPass,
+                    trigger: 'blur'
+                }]
+            }
+        }
+    },
+    methods: {
+        submitForm(formName) {
+            this.$refs[formName].validate(valid => {
+                if (valid) {
+                    let params = {
+                        oldPassword: this.ruleForm.oldPass,
+                        newPassword: this.ruleForm.newPass
+                    }
+                    this.$http
+                        .put(`/user/update/${sessionStorage.uid}`, params)
+                        .then(res => {
+                            this.$message.success('修改密码成功!请重新登录')
+                            sessionStorage.clear()
+                            setTimeout(() => {
+                                this.$router.push({
+                                    path: '/'
+                                })
+                            }, 1000)
+                        })
+                        .catch(err => {
+                            console.log('Error=>', err)
+                        })
+                } else {
+                    console.log('error submit!!')
+                    return false
+                }
+            })
+        },
+        resetForm(formName) {
+            this.$refs[formName].resetFields()
+        }
+    }
+}
+</script>
+
+<style scoped>
+.change-password {
+    width: 400px;
+    margin: auto;
+}
+</style>
