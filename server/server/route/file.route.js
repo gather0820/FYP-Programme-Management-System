5fd18f61af69ecd0ebc1957b64519c28f8246851
gather0820 <gather0820@users.noreply.github.com>
2020-04-13 21:12:21 +0100

    Create file.route.js

---
+++ server/server/route/file.route.js
@@ -0,0 +1,23 @@
+//  文件接口
+module.exports = function(app) {
+  const file = require('../controller/file.controller');
+
+  //  新增文件
+  app.post('/file/add', file.create);
+
+  //  删除文件
+  app.delete('/file/delete/:fileName/:fileId', file.delete);
+
+  // 下载文件
+  app.get('/file/download/:fileName/:fileId', file.download);
+
+  // 获取文件信息列表
+  app.post('/file/list', file.findAll);
+
+  app.post('/file/share/save/:uid',file.save);
+
+  //文件预览
+  app.get('/file/preview/:fileName',file.preview);
+
+  app.post('/file/transcode', file.transcode)
+};
