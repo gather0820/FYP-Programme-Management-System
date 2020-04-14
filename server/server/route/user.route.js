a323b5d5a8734ddace22b04ce07f61bab13f1342
gather0820 <gather0820@users.noreply.github.com>
2020-04-14 20:30:22 +0100

    Create user.route.js

---
+++ server/server/route/user.route.js
@@ -0,0 +1,26 @@
+//  用户
+module.exports = function(app) {
+  const user = require('../controller/user.controller');
+
+  //  新增用户
+  app.post('/user/add', user.create);
+
+  //  根据用户名和密码查询用户
+  app.post('/user/validate', user.validate);
+
+  //  修改密码
+  app.put('/user/update/:userId', user.updatePassWord);
+
+  //  根据用户名查询ID
+  app.get('/user/id/:name',user.getUserIdByName)
+
+  app.post('/user/verify',user.verifyEmail)
+
+  //测试前后端交互
+  app.get("/user/test",(req,res)=>{
+
+    res.send({
+      "status":'OK'
+    })
+  })
+};
