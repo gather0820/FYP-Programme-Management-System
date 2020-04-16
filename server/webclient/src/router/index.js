80207dc5508b57b6d5d594351ef0620d53cbde07
gather0820 <gather0820@users.noreply.github.com>
2020-04-16 20:30:28 +0100

    Create index.js

---
+++ server/webclient/src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import index from '@/components/index'
+import list from '@/components/tab-list'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: index
+    },
+    {
+      path: '/tab-list',
+      name: 'tab-list',
+      component: list
+    }
+  ],
+  mode: 'history'
+})
+//  校验登录
+router.beforeEach((to, from, next) => {
+  if (to.name === 'tab-list') {
+    if (!sessionStorage.username) {
+      window.alert('Session timeout, please login again!')
+      return window.location.replace('/')
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+export default router
