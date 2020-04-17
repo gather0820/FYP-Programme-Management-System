0c9ac2d12bf9fd75a9b434e05de2128e152200b5
gather0820 <gather0820@users.noreply.github.com>
2020-04-17 20:32:45 +0100

    Update settings.vue

--- webclient/src/components/settings.vue
+++ webclient/src/components/settings.vue
@@ -1,27 +1,26 @@
 <template>
-    <el-tabs  style="height: 500px;margin-left:30px;">
+<el-tabs style="height: 500px;margin-left:30px;">
     <el-tab-pane label="Change password">
         <user-set></user-set>
     </el-tab-pane>
     <el-tab-pane label="Storage space">Storage space</el-tab-pane>
     <el-tab-pane label="XXX">XXX</el-tab-pane>
     <el-tab-pane label="XXX">XXX</el-tab-pane>
-  </el-tabs>
+</el-tabs>
 </template>
 
 <script>
 import userSet from './user-set';
 export default {
-    name:"settings",
+    name: "settings",
     components: {
         userSet,
     },
-    data(){
-        return{
-            
+    data() {
+        return {
+
         };
     },
 
-    
 }
 </script>
