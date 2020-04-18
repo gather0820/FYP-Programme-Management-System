87c3313e876b1c9022cd1c056c80f9b66dde2819
gather0820 <gather0820@users.noreply.github.com>
2020-04-18 15:53:42 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
-
+app.set('trust proxy', 1)
 const path = require('path');
 
 const bodyParser = require('body-parser');
+
+let cookieParser = require('cookie-parser');
+let session = require('express-session');
+//app.use(cookieParser());
+app.use(session({
+  secret: 'secret', // 对session id 相关的cookie 进行签名
+  resave: true,
+  rolling:true,
+  saveUninitialized: false, // 是否保存未初始化的会话
+  cookie: {
+    maxAge: 1000 * 60 * 3, // 设置 session 的有效时间，单位毫秒
+  },
+  saveUninitialized: false,
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -22,8 +36,10 @@ app.use(bodyParser.json());
  */
 const cors = require('cors');
 const corsOptions = {
-  origin: '*',
-  optionSuccessStatus: 200
+  origin: 'http://localhost:8080',
+  optionSuccessStatus: 200,
+  credentials: true,
+  maxAge: '1728000'
 };
 app.use(cors(corsOptions));
 
@@ -39,7 +55,7 @@ const storage = multer.diskStorage({
   },
   filename(req, file, cb) {//获取文件名和扩展名
     const fileNameArr = file.originalname.split('.');
-    const fileName = `${md5(fileNameArr[0])}.${fileNameArr[1]}`;
+    const fileName = `${md5(fileNameArr[0])}.${fileNameArr[fileNameArr.length - 1]}`;//2020-03-25-v3
     cb(null, fileName);
   }
 });
@@ -47,18 +63,20 @@ app.use(multer({ storage }).any());
 
 const db = require('./config/db.config');
 
-const cookie = require('cookie-parser');
+
+
 
 /* db.sequelize.sync({ force: true }).then(() => {
   console.log('重建表');
 }); */
 require('./route/user.route')(app);
 require('./route/file.route')(app);
+require('./route/share.route')(app);
 
 //  创建服务器
 let server = app.listen(process.env.PORT || 8081, () => {
   //let host = server.address().address;
-  let host = '0.0.0.0';
+  let host = '127.0.0.1';
   let port = server.address().port;
   console.log('started at: http://%s:%s', host, port);
 });
