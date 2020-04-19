876288494db674dc0c6e290bdcdd915d20f71739
gather0820 <gather0820@users.noreply.github.com>
2020-04-19 20:31:44 +0100

    Update user.route.js

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -11,6 +11,12 @@ module.exports = function(app) {
   //  修改密码
   app.put('/user/update/:userId', user.updatePassWord);
 
+  //  根据用户名查询ID
+  app.get('/user/id/:name',user.getUserIdByName)
+
+  app.post('/user/verify',user.verifyEmail)
+
+  app.get('/user/checkcode',user.checkCode)
   //测试前后端交互
   app.get("/user/test",(req,res)=>{
 
