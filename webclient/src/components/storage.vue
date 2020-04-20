5205bdd0a01adf84e31f8fcfdb39ec1388af0a5b
gather0820 <gather0820@users.noreply.github.com>
2020-04-20 12:46:55 +0100

    Create storage.vue

---
+++ webclient/src/components/storage.vue
@@ -0,0 +1,99 @@
+<template>
+<ve-pie :data="chartData1" :settings="chartSettings"></ve-pie>
+</template>
+
+<script>
+export default {
+    data() {
+        this.chartSettings = {
+            dataType: function (v) {
+                return v + ' MB'
+            },
+            radius:110
+        }
+        return {
+            chartData1: {},
+            chartData: {},
+            fileData: [],
+            pdfFile: 1,
+            officeFile: 0,
+            images: 0,
+            others: 0,
+            total: 0,
+
+        }
+    },
+
+    methods: {
+        getAllFileData() {
+            let params = {
+                uid: sessionStorage.getItem("uid")
+            };
+            this.$http
+                .post("/file/list", params)
+                .then(res => {
+                    if (res.data.code === 0) {
+                        this.$message.error(res.data.msg);
+                    } else {
+                        this.fileData = res.data;
+                    }
+                }).then(() => {
+                    this.reorganizeData();
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        },
+        reorganizeData() {
+            let originalData = this.fileData;
+            let office = ['.doc', '.docx', '.ppt', '.pptx', '.xls', '.xlsx'];
+            let images = ['.jpg', '.png', '.bmp', '.tiff', '.gif']
+            originalData.forEach(e => {
+                let size = parseInt(e.size);
+                let type = e.type;
+                this.total += size;
+                if (type == '.pdf') {
+                    this.pdfFile += size;
+                    
+                } else if (office.includes(type)) {
+                    this.officeFile += size;
+                } else if (images.includes(type)) {
+                    this.images += size;
+                } else {
+                    this.others += size
+                }
+            });
+            this.chartData1 = {
+                columns: ['type', 'space usage'],
+                rows: [{
+                        'type': 'pdf documents',
+                        'space usage': (this.pdfFile/(1024*1024)).toFixed(2)
+                    },
+                    {
+                        'type': 'office documents',
+                        'space usage': (this.officeFile/(1024*1024)).toFixed(2)
+                    },
+                    {
+                        'type': 'images',
+                        'space usage': (this.images/(1024*1024)).toFixed(2)
+                    },
+                    {
+                        'type': 'other files',
+                        'space usage': (this.others/(1024*1024)).toFixed(2)
+                    },
+                    {
+                        'type': 'available',
+                        'space usage': ((5 * 1024 - this.total / (1024 * 1024))).toFixed(2)
+                    }
+                ]
+            }
+            
+
+        }
+
+    },
+    mounted() {
+        this.getAllFileData();
+    }
+}
+</script>
