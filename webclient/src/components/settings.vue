8bd8ede32f8e824f0f2659a71b4aab7fb5533ffb
gather0820 <gather0820@users.noreply.github.com>
2020-04-20 12:46:57 +0100

    Update settings.vue

--- webclient/src/components/settings.vue
+++ webclient/src/components/settings.vue
@@ -1,24 +1,34 @@
 <template>
-<el-tabs style="height: 500px;margin-left:30px;">
-    <el-tab-pane label="Change password">
-        <user-set></user-set>
+<el-tabs style="height: 500px;margin-left:30px;" v-model="tabActivedName">
+    <el-tab-pane v-for="(item, index) in componentList" :key="index" :label="item.tabLabel" :name="item.tabName">
+        <component :is="item.compoName" v-if="tabActivedName===item.tabName"></component>
     </el-tab-pane>
-    <el-tab-pane label="Storage space">Storage space</el-tab-pane>
-    <el-tab-pane label="XXX">XXX</el-tab-pane>
-    <el-tab-pane label="XXX">XXX</el-tab-pane>
 </el-tabs>
 </template>
 
 <script>
 import userSet from './user-set';
+import storage from './storage';
 export default {
     name: "settings",
     components: {
         userSet,
+        storage
     },
     data() {
         return {
-
+            tabActivedName: 'first',
+            componentList: [{
+                    tabName: 'first',
+                    compoName: 'user-set',
+                    tabLabel: 'Change Password'
+                },
+                {
+                    tabName: 'second',
+                    compoName: 'storage',
+                    tabLabel: 'Storage Space'
+                },
+            ],
         };
     },
 
