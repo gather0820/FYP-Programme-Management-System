5d78ced9d67c01adfb586c0b18b3f4e855191e76
gather0820 <gather0820@users.noreply.github.com>
2020-04-20 12:47:04 +0100

    Update main.js

--- webclient/src/main.js
+++ webclient/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VCharts from 'v-charts'
 import App from './App';
 import router from './router';
 
@@ -11,7 +12,8 @@ import 'element-ui/lib/theme-chalk/index.css';
 Vue.prototype.$http = http;
 
 Vue.config.productionTip = false;
-Vue.use(ElementUI)
+Vue.use(VCharts);
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
