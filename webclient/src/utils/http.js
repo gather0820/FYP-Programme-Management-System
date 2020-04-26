f5312b9cebc063cc4d7dc1bd90bb7fe008004217
gather0820 <gather0820@users.noreply.github.com>
2020-04-26 18:48:15 +0100

    Update http.js

--- webclient/src/utils/http.js
+++ webclient/src/utils/http.js
@@ -15,6 +15,18 @@ httpInstance.formurl = (url, data, config) => {
   })
 };
 
+/** 拦截器 interceptors
+ * 一个对所有的http请求过滤的功能:
+所有的请求需要加上token再发送给服务器.
+服务器根据token判断用户身份是否有效，响应也需要过滤，判断是否授权，进行统一处理。
+*
+ * A function that filters all HTTP requests:
+All requests need a token before they are sent to the server.
+The server determines the validity of the user's identity based on the token, 
+and the response needs to be filtered to determine whether it is authorized or not, 
+for uniform processing.
+ */
+
 //  request拦截器
 httpInstance.interceptors.request.use(
   config => {
