8359883ba449a35736f251719a5e1df905166bcf
gather0820 <gather0820@users.noreply.github.com>
2020-04-28 16:55:55 +0100

    Update header.vue

--- webclient/src/layout/header.vue
+++ webclient/src/layout/header.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="header">
+    <div class="left">
+      <div id="logo">
+      </div>
     <div class="title">
       SDMS
-      <span class="label">version.5</span>
+      <span class="label">version.6</span>
     </div>
+    </div>
+    
     <div class="user">
       User: {{username}}&nbsp;&nbsp;
       <a href="#" @click="signOut">&nbsp;&nbsp;&nbsp;&nbsp;Log out</a>
@@ -30,17 +35,37 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.left{
+  width: 500px;
+  padding: 0;
+  margin-left: 20px;
+}
+#logo {
+  background-image: url('../../static/logo.png');
+  background-repeat:no-repeat;
+  background-size: auto 100%;
+  position: fixed;
+  margin-bottom: 0em;
+  width: 100px;
+  height: 90px;
+
+  margin-left: 20px;
+}
 .header {
   text-align: center;
   font-size: 18px;
   margin-top: 20px;
   box-shadow: 0 5px 15px -10px #ccc;
-  padding: 20px;
+  padding-bottom: 20px;
 }
 .title {
   font-size: 34px;
+  height: 80px;
   text-align: left;
-  margin-left: 40px;
+  
+  margin-left: 130px;
+  padding-bottom: 0;
+  line-height: 130px;
 }
 .label {
   font-size: 25px;
