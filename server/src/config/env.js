b3c8fbaed73a056df13e9be27c538e44d2623ee5
gather0820 <gather0820@users.noreply.github.com>
2020-04-29 20:32:06 +0100

    Create env.js

---
+++ server/src/config/env.js
@@ -0,0 +1,16 @@
+//  数据库连接参数
+const env = {
+  database: 'SDMS',
+  username: 'root',
+  //host: '192.168.31.236',
+  password: '19980820.John',
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+module.exports = env;
