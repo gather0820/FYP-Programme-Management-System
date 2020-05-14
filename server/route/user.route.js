cb66304f3b0448fb32de0d52fa528259b507db6d
gather0820 <gather0820@users.noreply.github.com>
2020-05-14 20:34:03 +0100

    Update user.route.js

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -2,22 +2,22 @@
 module.exports = function(app) {
   const user = require('../controller/user.controller');
 
-  //  新增用户
+  //  新增用户 add user
   app.post('/user/add', user.create);
 
-  //  根据用户名和密码查询用户
+  //  根据用户名和密码查询用户 Query users by username and password
   app.post('/user/validate', user.validate);
 
-  //  修改密码
+  //  修改密码 change password
   app.put('/user/update/:userId', user.updatePassWord);
 
-  //  根据用户名查询ID
+  //  根据用户名查询ID  Query ID by user name
   app.get('/user/id/:name',user.getUserIdByName)
 
   app.post('/user/verify',user.verifyEmail)
 
   app.get('/user/checkcode',user.checkCode)
-  //测试前后端交互
+  //测试前后端交互  test
   app.get("/user/test",(req,res)=>{
 
     res.send({
