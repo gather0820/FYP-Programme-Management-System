f6d784a87c9f17cb23e80af963c3abcef02d8053
gather0820 <gather0820@users.noreply.github.com>
2020-05-14 20:34:08 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 app.set('trust proxy', 1)
 const path = require('path');
-
 const bodyParser = require('body-parser');
 
 let cookieParser = require('cookie-parser');
 let session = require('express-session');
 //app.use(cookieParser());
+
 app.use(session({
-  secret: 'secret', // 对session id 相关的cookie 进行签名
+  secret: 'secret', // 对session id 相关的cookie 进行签名 Signature of session ID-related cookies
   resave: true,
   rolling:true,
   saveUninitialized: false, // 是否保存未初始化的会话
@@ -21,18 +21,8 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-// app.all('*', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:8081/user/add");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-//   res.header("X-Powered-By",' 3.2.1')
-//   res.header("Content-Type", "application/json;charset=utf-8");
-//   next();
-// });
-
 /**
- * 解决跨域
+ * 解决跨域 cross-domain
  */
 const cors = require('cors');
 const corsOptions = {
@@ -53,9 +43,9 @@ const storage = multer.diskStorage({
   destination(req, res, cb) {
     cb(null, './resource');
   },
-  filename(req, file, cb) {//获取文件名和扩展名
+  filename(req, file, cb) {//获取文件名和扩展名 Get file name and extension. Encryption using MD5
     const fileNameArr = file.originalname.split('.');
-    const fileName = `${md5(fileNameArr[0])}.${fileNameArr[fileNameArr.length - 1]}`;//2020-03-25-v3
+    const fileName = `${md5(fileNameArr[0])}.${fileNameArr[fileNameArr.length - 1]}`; 
     cb(null, fileName);
   }
 });
@@ -63,16 +53,10 @@ app.use(multer({ storage }).any());
 
 const db = require('./config/db.config');
 
-
-
-
-/* db.sequelize.sync({ force: true }).then(() => {
-  console.log('重建表');
-}); */
 require('./route/user.route')(app);
 require('./route/file.route')(app);
 require('./route/share.route')(app);
 
-//创建服务器
+//创建服务器 server creation
 module.exports = app.listen(process.env.PORT || 8081, () => {
 });
